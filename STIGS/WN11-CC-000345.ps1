bc926687c0cef7236f661bb229d36c517c1ec88a
Chris Eben <37830396+ashuchris@users.noreply.github.com>
2026-09-08 21:49:46 -0400

    Create WN11-CC-000345.ps1

---
+++ STIGS/WN11-CC-000345.ps1
@@ -0,0 +1,48 @@
+<#
+.SYNOPSIS
+    The Windows Remote Management (WinRM) service must not use Basic authentication.
+
+.NOTES
+    Author          : Chris Eben
+    LinkedIn        : https://www.linkedin.com/in/ashuchris/
+    GitHub          : github.com/ashuchris
+    Date Created    : 2026-09-08
+    Last Modified   : 2026-09-08
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000345
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\WN11-CC-000345.ps1 
+#>
+
+# STIG Remediation: Disable WinRM Basic Authentication
+# AllowBasic = 0
+
+$RegistryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WinRM\Service"
+$ValueName = "AllowBasic"
+$ValueData = 0
+
+# Create the registry key if it does not exist
+if (-not (Test-Path $RegistryPath)) {
+    New-Item -Path $RegistryPath -Force | Out-Null
+}
+
+# Create or update the registry value
+New-ItemProperty `
+    -Path $RegistryPath `
+    -Name $ValueName `
+    -PropertyType DWord `
+    -Value $ValueData `
+    -Force | Out-Null
+
+Write-Host "STIG remediation complete: AllowBasic = 0"
